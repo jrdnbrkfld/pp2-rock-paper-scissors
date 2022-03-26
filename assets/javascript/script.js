ebb574441ffb6843b80e2a86ceb51fc221994bd3
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-03-26 19:17:21 +0000

    add function to generate the result of who wins or if it is a draw

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -26,3 +28,28 @@ function generateComputerChoice() {
     }
     computerChoiceDisplay.innerHTML = computerChoice
 }
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = "You win!"
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = "You lost!"
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = "You win!"
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = "You lose!"
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = "You win!"
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = "You lose!"
+    }
+    resultDisplay.innerHTML = result
+} 
