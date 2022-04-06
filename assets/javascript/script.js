ef0914d9d4349f015540975a2bf63367fe5eb96d
jrdnbrkfld <jrdnbrkfld@icloud.com>
2022-04-06 09:39:12 +0000

    added missing semi colons throughout the javascript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,14 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
 const splash = document.querySelector('.splash');
-let userChoice
-let computerChoice
-let result
+let userChoice;
+let computerChoice;
+let result;
 const gameOver = 5;
-let userScore = document.getElementById('user-score')
-let computerScore = document.getElementById('computer-score')
+let userScore = document.getElementById('user-score');
+let computerScore = document.getElementById('computer-score');
 
 // start screen
 
@@ -16,66 +16,66 @@ document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash.classList.add('display-none');
     }, 5000);
-})
+});
 
 // event listener to generate computer choice when user makes a choice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
 }))
 
 // generates the computers choice as a random number
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1
-    console.log(randomNumber)
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
 
     if (randomNumber == 1) {
-        computerChoice = 'rock'
+        computerChoice = 'rock';
     }
     if (randomNumber == 2) {
-        computerChoice = 'paper'
+        computerChoice = 'paper';
     }
     if (randomNumber == 3) {
-        computerChoice = 'scissors'
+        computerChoice = 'scissors';
     }
-    computerChoiceDisplay.innerHTML = computerChoice
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 // generates the result 
 
 function getResult() {
     if (computerChoice == userChoice) {
-        result = "It's a draw!"
+        result = "It's a draw!";
     }
     if (computerChoice == 'rock' && userChoice === "paper") {
-        result = "You win!"
-        incrementUserScore()
+        result = "You win!";
+        incrementUserScore();
     }
     if (computerChoice == 'rock' && userChoice === "scissors") {
-        result = "You lost!"
-        incrementComputerScore()
+        result = "You lost!";
+        incrementComputerScore();
     }
     if (computerChoice == 'paper' && userChoice === "scissors") {
-        result = "You win!"
-        incrementUserScore()
+        result = "You win!";
+        incrementUserScore();
     }
     if (computerChoice == 'paper' && userChoice === "rock") {
-        result = "You lost!"
-        incrementComputerScore()
+        result = "You lost!";
+        incrementComputerScore();
     }
     if (computerChoice == 'scissors' && userChoice === "rock") {
-        result = "You win!"
-        incrementUserScore()
+        result = "You win!";
+        incrementUserScore();
     }
     if (computerChoice == 'scissors' && userChoice === "paper") {
-        result = "You lost!"
-        incrementComputerScore()
+        result = "You lost!";
+        incrementComputerScore();
     }
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
 }
 
 // generates score for player 
@@ -85,7 +85,7 @@ function incrementUserScore() {
     currentUserScore++;
     console.log(currentUserScore);
     userScore.innerText = currentUserScore;
-    checkWinner(currentUserScore, 'user')
+    checkWinner(currentUserScore, 'user');
 }
 
 function incrementComputerScore() {
@@ -93,7 +93,7 @@ function incrementComputerScore() {
     currentComputerScore++;
     console.log(currentComputerScore);
     computerScore.innerText = currentComputerScore;
-    checkWinner(currentComputerScore, 'comp')
+    checkWinner(currentComputerScore, 'comp');
 }
 
 // alerts who the winner is based on who reaches 5 first 
